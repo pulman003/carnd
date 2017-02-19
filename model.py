33af22aa67198c843cf8cdf8bac66353f6e7c203
nick <nick@nite.com>
2017-02-19 09:25:26 -0700

    cleanup and comments

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import pickle
 import tensorflow as tf
 import numpy as np
 import os
@@ -7,31 +6,34 @@ import cv2
 import sklearn
 
 from sklearn.model_selection import train_test_split
-from keras.layers import Dense, Dropout, Flatten, Lambda, Activation
+from keras.layers import Dense, Dropout, Flatten, Lambda, ELU
 from keras.layers import Convolution2D
 from keras.models import Sequential
 from keras.models import load_model
 
+# this is used because of a bug between keras and tensorflow
 tf.python.control_flow_ops = tf
 
+# Set up command line arguments
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
-# Flags to allow me to select data, models, epochs and batch sizes
 flags.DEFINE_string('data', '', 'Data path')
 flags.DEFINE_string('model', '', 'Model path')
 flags.DEFINE_integer('epochs', 5, 'Number of epochs')
-flags.DEFINE_integer('batch_size', 256, 'Batch size')
+flags.DEFINE_integer('batch_size', 32, 'Batch size')
 
+# read in csv log file
 samples = []
-# Open the data folder and read in data from driving_log.csv
 with open(FLAGS.data+'driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
 
+# split into training and validation set
 train_samples, validation_samples = train_test_split(samples, test_size = 0.2)
 
+# set up generator (this was basically copied from the generators lecture)
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:
@@ -41,7 +43,6 @@ def generator(samples, batch_size=32):
 
             images = []
             angles = []
-            # Open the image and do some preprocessing (only look at the bottom half
             for batch_sample in batch_samples:
                 name = FLAGS.data+'IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
@@ -49,57 +50,59 @@ def generator(samples, batch_size=32):
                 images.append(center_image)
                 angles.append(center_angle)
             X_train = np.array(images)
+	    # cut off top half of image
             X_train = X_train[:,80:,:,:]
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-# Model follows the NVidia model with minor edits to dropout
-def get_model():
-  ch, row, col = 3, 80, 320  # camera format
-  
+# set up model
+def get_model(time_len=1):
+  ch, row, col = 3, 80, 320  # camera format (image halved)
+
   model = Sequential()
   model.add(Lambda(lambda x: x/127.5 - 1.,
             input_shape=(row, col, ch),
             output_shape=(row, col, ch)))
   model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="same"))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode="same"))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode="same"))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Convolution2D(64, 3, 3, border_mode="same"))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Convolution2D(64, 3, 3, border_mode="same"))
   model.add(Flatten())
   model.add(Dropout(0.2))
-  model.add(Activation('relu'))
-  model.add(Dense(1164))
-  model.add(Dropout(0.2))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Dense(100))
   model.add(Dropout(0.2))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Dense(50))
   model.add(Dropout(0.2))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Dense(10))
   model.add(Dropout(0.2))
-  model.add(Activation('relu'))
+  model.add(ELU())
   model.add(Dense(1))
 
   model.compile(optimizer="adam", loss="mse")
   return model
 
+# create generators
 train_generator = generator(train_samples, batch_size=FLAGS.batch_size)
 validation_generator = generator(validation_samples, batch_size = FLAGS.batch_size)
 
-# This allows me to revist and load previous models
+# load in the model if selected in the flags, otherwise train new network
 model_path = FLAGS.model
 if model_path:
     model = load_model(model_path)
     print("Loaded")
 else:
     model = get_model()
+    
+# fit the model
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples), nb_epoch=FLAGS.epochs, validation_data=validation_generator, nb_val_samples = len(validation_samples))
 
+# save file
 model.save('model.h5')
